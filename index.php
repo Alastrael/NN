d5edba4ae6df372e6e93d0fcc00cce0d5d98a7c3
Alastrael <michaelmartin.92140@gmail.com>
2017-11-25 22:23:14 +0100

    re

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" type="text/css" href="accueil.css">
+    <link rel="stylesheet" type="text/css" href="vues/accueil.css">
 
 
     <link rel="stylesheet" type="text/css" href="doc/css/bootstrap.min.css">
