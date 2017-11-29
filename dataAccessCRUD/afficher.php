55eec70c108d97098ef2d14f84ed8f68f61c2d33
Alastrael <michaelmartin.92140@gmail.com>
2017-11-29 19:00:51 +0100

    real

--- dataAccessCRUD/afficher.php
+++ dataAccessCRUD/afficher.php
@@ -8,8 +8,10 @@
         $user = "root";
         $mdp = "";
 
-        $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $mdp)
+        $pdo=new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'))
             or die ("Problème de connexion à la base de donnée");
+
+           
 
         return $pdo;
 
@@ -21,16 +23,39 @@
 
         $pdo = connexion();
 
-        $requete = "select contenu_formation from formation order by id_Formation";
-        $counter = "select count(contenu_formation) from formation";
-
+        $requete = "select * from formation order by id_Formation";
+      
         $execRequete = $pdo->query($requete);
-        $execCounter = $pdo->query($counter);
 
         $data = $execRequete->fetchAll();
 
         foreach ($data as $valeur) {
-            echo "<a class='list-group-item list-group-item-action'>".$valeur['contenu_formation']."</a>";
+            echo 
+            "
+            <a class='list-group-item list-group-item-action' data-toggle='collapse' href='#"
+            .$valeur['id_Formation']."' aria-expanded='false' aria-controls='collapseExample'>
+
+            ".$valeur['nom_formation']."  
+                <i class='fa fa-arrow-circle-down' aria-hidden='true'></i>
+            </a> 
+            
+            <div class='collapse' id='".$valeur['id_Formation']."'>
+                <div class='card card-body'> 
+                    ".$valeur['contenu_formation'].
+                    "<br><br>
+                    Qui commencera le : ".$valeur['Date_formation']."
+                    <br><br>
+                    Elle durera : ".$valeur['Duree_formation']." sur ".$valeur['NbrJour_formation']
+                    .". Cette formation se déroulera à/en ".$valeur['lieu_formation']."
+                    <br><br>
+                    Prérequis pour la formation : ".$valeur['prerequis_formation']."
+                    <br><br>
+                    <button id='print' type='print' class='button'>
+                    Imprimer
+                    </button>
+                </div>
+            </div>
+            ";
         } 
 
 
